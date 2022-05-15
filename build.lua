c91eec216d44874fdad709fea424cf898b4f57ce
well-in-that-case <ryanmoople@gmail.com>
2022-05-15 17:20:07 -0400

    add accurate profiler

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 local COMPILER = "mingw32-g++"
 local OBJECT_NAME = "profiler"
-local HEADER_PATH = [[]]
-local LIBRARY_PATH = [[]]
+local HEADER_PATH = [[C:\Users\DEMSK\Desktop\Lua Binaries\source\src]]
+local LIBRARY_PATH = [[C:\Users\DEMSK\Desktop\Lua Binaries/lua54.dll]]
 
 local c1 = [[%s -O2 -c -o %s.o -I"%s" %s.cpp]]
 local c2 = [[%s -shared -o %s.dll %s.o "%s" -lm]]
---
+++ profiler.cpp
@@ -0,0 +1,50 @@
+/*
+MIT License
+
+Copyright (c) 2022 Ryan Starrett
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+    'profiler.cpp' is a small Lua extension module, tailored to my OS for high-resolution benchmarking.
+    All the contents in this file are licensed under the MIT License, found above, or in the LICENSE file.
+*/
+
+#include <lua.hpp>
+#include <Windows.h>
+
+static int once(lua_State *L) {
+    LARGE_INTEGER frequency; QueryPerformanceFrequency(&frequency);
+    LARGE_INTEGER start; QueryPerformanceCounter(&start);
+    lua_call(L, lua_gettop(L) - 1, 0);
+    LARGE_INTEGER end; QueryPerformanceCounter(&end);
+    lua_pushnumber(L, static_cast<double>(end.QuadPart - start.QuadPart) / frequency.QuadPart);
+    return 1;
+}
+
+static const struct luaL_Reg exports[] = {
+    {"once", once},
+    {NULL, NULL}
+};
+
+extern "C" __declspec(dllexport) int luaopen_profiler(lua_State *L) {
+    luaL_newlib(L, exports);
+    return 1;
+}
